14ecbab31b25df31f41976ced471ddbc8e75720e
symbolican.com@gmail.com <symbolican.com@gmail.com@f5dde273-9c4f-1969-2b65-d89bc31a4337>
2012-03-30 13:19:57 +0000

    creating tags/cloudrun-0.9

---
+++ cloudrun/tags/cloudrun-0.9/kvm/cloudenv.sh
@@ -0,0 +1,9 @@
+#Alternate url for download
+ALTURL=kvmhost/cloudrun
+#Set AWS metadata InstanceID
+AWSInstanceID=KVM-QEMU01
+#Stop publish metrics to AWS Cloudwatch
+StopAWSCloudwatch=True
+#Stop publish message to AWS SNS
+StopAWSSNS=True
+
